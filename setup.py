7cc6edbb1233c5797091dd2f1bf9676508c60f86
sanika jadhav <sanika@gmail.com>
2025-04-11 14:44:40 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'FlyYara',
+    version= '0.0.0',
+    author= 'Sanika & Saif',
+    packages= find_packages(),
+    install_requires = []
+
+)
